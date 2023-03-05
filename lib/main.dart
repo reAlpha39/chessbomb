369bac7234f6c9a141167e05c21c266d630783fb
reAlpha39 <yunusw718@gmail.com>
2023-03-05 11:33:47 +0700

    define fetching font on runtime

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:bombernyaa/injection.dart';
 import 'package:bombernyaa/presentation/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  GoogleFonts.config.allowRuntimeFetching = true;
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
